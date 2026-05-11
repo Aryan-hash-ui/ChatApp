0a1a90aa46a257302b0a49e98dddac86bbdbe288
Aryan Gupta <aryangupta1904@gmail.com>
2026-05-11 12:28:56 +0530

    Update CORS origin for socket.io server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. CORS will allow more than one app running on different ports 
 
 const io = require("socket.io")(8000, {
-    cors: "https://chatapp-0z4o.onrender.com"
+    cors: "https://chatapp-l6ad.onrender.com"
     // cors:"*"
 })
 let users = {}
